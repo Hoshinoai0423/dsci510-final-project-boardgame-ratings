97a9cc36057375b5cb3a72604ef03f60902b6dd0
Hoshinoai0423 <145949953+Hoshinoai0423@users.noreply.github.com>
2026-04-22 19:28:59 +0800

    update configs

---
+++ src/configs.py
@@ -0,0 +1,13 @@
+# Paths
+DATA_DIR = "../data"
+RESULTS_DIR = "../results"
+
+# Files
+BGG_RANKS_FILE = "boardgames_ranks.csv"
+FINAL_DATA_FILE = "final_top500_games.csv"
+
+
+
+# URLs
+BGG_RANKS_URL = "https://boardgamegeek.com/data_dumps/bg_ranks"
+BGG_API_URL = "https://boardgamegeek.com/xmlapi2/thing"
